87891e69f88b2b529a44e8c3c69f4cf317001a35
NajilBu <najilbumacod12@gmail.com>
2026-02-17 21:09:43 +0800

    picture take

---
+++ S_Project/save.php
@@ -0,0 +1,17 @@
+<?php
+
+$folder = "pictures/";
+
+if(!file_exists($folder)){
+    mkdir($folder, 0777, true);
+}
+
+$filename = $folder . "photo_" . time() . ".png";
+
+if(move_uploaded_file($_FILES['photo']['tmp_name'], $filename)){
+    echo "Photo saved as: " . $filename;
+} else {
+    echo "Error saving photo.";
+}   
+
+?>
